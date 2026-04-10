80fff1a8ec8740ec7b2012de2bf3cc93d8727456
Strictlywoo <lpalma50771@ufide.ac.cr>
2026-04-09 18:39:03 -0600

    Semana 11 - 12

--- src/main/java/com/tiendaTech/tienda/repository/UsuarioRepository.java
+++ src/main/java/com/tiendaTech/tienda/repository/UsuarioRepository.java
@@ -4,13 +4,21 @@ import com.tiendaTech.tienda.domain.Usuario;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    
+
     public Optional<Usuario> findByUsernameAndActivoTrue(String username);
-    
+
+    public List<Usuario> findByActivoTrue();
+
+    public Optional<Usuario> findByUsername(String username);
+
+    public Optional<Usuario> findByUsernameAndPassword(String username, String Password);
+
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+
+    public boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
-
-
-
